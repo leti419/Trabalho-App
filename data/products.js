bf81632f95e25240110549c261a89cdd97ed06bf
leti419 <leticiamilenesilva09@gmail.com>
2025-06-24 22:32:31 -0300

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,9 @@
+
+const products = [
+    { id: '1', name: 'Café Expresso', price: 5 },
+    { id: '2', name: 'Cappuccino', price: 8 },
+    { id: '3', name: 'Pão de Queijo', price: 4 },
+    { id: '4', name: 'Bolo de Cenoura', price: 6 },
+  ];
+
+  export default products;
